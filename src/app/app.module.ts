dd951a24571d0f251f16ccebca33070505986153
Amilton Machado <amilton.machado@fw7.com.br>
2021-09-15 22:18:50 -0300

    Build: 202109152218

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card'
 
 import { HeaderComponent } from './components/templates/header/header.component'
 import { NavComponent } from './components/templates/nav/nav.component'
 import { FooterComponent } from './components/templates/footer/footer.component'
+import { HomeComponent } from './views/home/home.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
